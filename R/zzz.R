19584d10836b466ce01e27aef103a2e739c6b648
Markus Gesmann <markus.gesmann@gmail.com>
2010-12-19 00:00:00 +0000

    version 0.2.3

--- R/zzz.R
+++ R/zzz.R
@@ -22,10 +22,43 @@
 {
   setMethod("toJSON", "Date",
             function(x, container = length(x) > 1 || length(names(x)) > 0, ...) {
-              tmp <- format(as.Date(x),"new Date(%Y,%m,%d)")
+              dt <- as.Date(x)
+              y <- format(dt,"%Y")
+              m <- as.numeric(format(dt,"%m")) -1
+              d <- as.numeric(format(dt,"%d"))
+              
+              tmp <- paste("new Date(",y,",",m,",",d,")",sep="")
               paste(tmp, collapse=", ")
             })
   
+  setMethod("toJSON", "POSIXct",
+            function(x, container = length(x) > 1 || length(names(x)) > 0, ...) {
+              dt <- as.POSIXct(x)
+              y <- format(dt,"%Y")
+              m <- as.numeric(format(dt,"%m")) -1
+              d <- as.numeric(format(dt,"%d"))
+              H <- as.numeric(format(dt,"%H"))
+              M <- as.numeric(format(dt,"%M"))
+              S <- as.numeric(format(dt,"%S"))
+              
+              tmp <- paste("new Date(",y,",",m,",",d,",",H,",",M,",",S,")",sep="")
+              paste(tmp, collapse=", ")
+            })
+  
+  setMethod("toJSON", "POSIXlt",
+            function(x, container = length(x) > 1 || length(names(x)) > 0, ...) {
+              dt <- as.POSIXlt(x)
+              y <- format(dt,"%Y")
+              m <- as.numeric(format(dt,"%m")) -1
+              d <- as.numeric(format(dt,"%d"))
+              H <- as.numeric(format(dt,"%H"))
+              M <- as.numeric(format(dt,"%M"))
+              S <- as.numeric(format(dt,"%S"))
+              
+              tmp <- paste("new Date(",y,",",m,",",d,",",H,",",M,",",S,")",sep="")
+              paste(tmp, collapse=", ")
+            })
+
   library(utils)
   packageStartupMessage(gvisWelcomeMessage())    
   
@@ -35,9 +68,9 @@
 gvisWelcomeMessage <- function(){
   
        paste("\nWelcome to googleVis version ", packageDescription("googleVis")$Version, "\n\n",
-      "Type ?googleVis to see the overall documentation and\n",
-      "vignette('googleVis') to see the package vignette.\n",
-      "You can see a demo of the package via: demo(googleVis)\n\n",  
+      "Type ?googleVis to access the overall documentation and\n",
+      "vignette('googleVis') for the package vignette.\n",
+      "You can execute the demo of the package via: demo(googleVis)\n\n",  
       "More information is available on the googleVis project web-site:\n",
                         "http://code.google.com/p/google-motion-charts-with-r/\n\n",
       
